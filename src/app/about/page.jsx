d7629251445bb766eae61959e6bf026a820ef56b
nahidul-fahim <nahidulfahim.official@gmail.com>
2024-02-17 20:58:03 +0600

    explored some hooks of next js

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,11 +1,18 @@
+"use client"
+import Image from "next/image";
+import { useSearchParams } from "next/navigation";
+import aboutImg from "../../../public/next js.png"
 
+const Page = () => {
+    const searchParams = useSearchParams();
+    console.log(searchParams);
 
-const page = () => {
     return (
         <div>
             <h2>About page</h2>
+            <Image src={aboutImg} alt="About page image" priority={true} width={500} height={500}></Image>
         </div>
     );
 };
 
-export default page;
+export default Page;
--- src/components/navbar/navlink.jsx
+++ src/components/navbar/navlink.jsx
@@ -6,9 +6,6 @@ const NavLink = ({ item }) => {
 
     // get hooks
     const pathName = usePathname();
-    console.log(pathName)
-
-
 
     return (
         <Link href={item.path} className={`${pathName === item.path ? "border-b-2 border-white duration-500" : "border-b-2 border-[#ffffff00]"}`}>{item.title}</Link>
